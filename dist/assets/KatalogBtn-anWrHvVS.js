5a6cfa904828d503e5f1100a31eae04dd12b4338
IT-Gleb <mygit85@gmail.com>
2024-03-03 00:46:17 +0300

    static build add...

--- dist/assets/KatalogBtn-anWrHvVS.js
+++
@@ -1,1 +0,0 @@
-import{d as e,b as n,i as a,D as r}from"./index-_itgRtIZ.js";const s={class:"border border-col1-100 text-col1-100 px-12 py-4 font-roboto text-xl w-[176px] h-[63px]"},i=e({__name:"KatalogBtn",props:{title:{}},setup(t){const o=t;return(c,l)=>(n(),a("button",s,r(o.title),1))}});export{i as default};
