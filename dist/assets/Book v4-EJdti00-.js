ec0cf379865dd0d3ba57e33eee5fc7a82b3f2f6a
IT-Gleb <mygit85@gmail.com>
2024-03-03 15:34:22 +0300

    final 11

--- dist/assets/Book v4-EJdti00-.js
+++
@@ -1,1 +0,0 @@
-const s="/PaperSite/assets/Folder v8 Open 1-23DEkLyA.png",e="/PaperSite/assets/Book v4-WxzK82Cd.png";export{s as _,e as a};
--- src/Router/mRouter.ts
+++ src/Router/mRouter.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 
 const appRoutes = [
   {
-    path: "/PaperSite/",
+    path: "/Paper/",
     component: () => import("../components/Pages/MainPage.vue"),
   },
   {
